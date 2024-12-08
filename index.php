7c31e8dd5aefc328a05f4aad5ac6c666abdb041a
micdesgalier <desgaliermichael@gmail.com>
2024-12-08 17:10:53 +0100

    Correction variables de config

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ ini_set('display_errors', 'on');
 //define('DB_NAME', 'todolist');
 //define('DB_HOST', '127.0.0.1');
 //define('DB_PORT', '3306');
-define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '');
+define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
 define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
